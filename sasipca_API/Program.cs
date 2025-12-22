507acf0cbd8f3396a470c1661a01a0d945a90b10
Miguel MA. Areal <miguel.areal04@gmail.com>
2025-12-22 09:32:46 +0000

    Debug

--- sasipca_API/Program.cs
+++ sasipca_API/Program.cs
@@ -322,6 +322,14 @@ namespace sasipca_API
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
             });
 
+            // --- DEBUG: Escreve no log todos os pedidos que chegam ---
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine($"[DEBUG] Recebi pedido: {context.Request.Path}");
+                await next();
+            });
+            // ---------------------------------------------------------
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
